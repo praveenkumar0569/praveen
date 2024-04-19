4a8e9be2c9035b8e6ab2e62ee9fc3dbbe8db35bf
praveenkumar0569 <praveen.kumar@sirigiritrainings.onmicrosoft.com>
2024-04-19 21:02:19 +0530

    speak

--- src/Test/TestHello.js
+++ src/Test/TestHello.js
@@ -1,29 +1,23 @@
 import React from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+const TestHello = () => {
+ const startlisten=()=> SpeechRecognition.startListening({ continuous: true,language:'en-IN' })
+  const { transcript, browserSupportsSpeechRecognition,resetTranscript} = useSpeechRecognition()
+  
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
 
-const TestHello = ({name}) => {
   return (
-    <div>
-      <form>
-        
-<label htmlFor="name">Name</label>
-<input type="text" id="name" />
-<textarea ></textarea>
-< label htmlFor="job-location"> Job location</label>
-<select id="job-location">
-<option value="">Select a country</option>
-<option value="US">United States</option>
-<option value="GB">United Kingdom</option>
-<option value="CA"> Canada</option>
-<option value="IN"> India</option> 
-<option value="AU">Australia</option>
-</ select>
-
-< label>
-<input type="checkbox" id="terms" /> I agree to the terms and conditions
-</ label>
-
-< button>Submit</button>
-</form>
+    <div className='container'>
+      <div className='textarea'>
+        {transcript}
+      </div>
+      <div>
+   <button onClick={resetTranscript}>Reset</button>
+   <button onClick={startlisten}>Start</button>
+   <button onClick={SpeechRecognition.stopListening}>Stop</button>
+   </div>
     </div>
   )
 }
--- src/Test/TestHello.test.js
+++ src/Test/TestHello.test.js
@@ -2,15 +2,60 @@ import { render, screen } from "@testing-library/react"
 import TestHello from "./TestHello"
 
 describe("application test",()=>{
+   
     test("renders",()=>{
+
         render(<TestHello/>)
+        const headers=screen.getByRole('heading',{
+            name:"praveen"
+        });
+        expect(headers).toBeInTheDocument()
+        //this is getbyrole
         const text=screen.getByRole('textbox',{
             name:"Name",
         });
+       
         expect(text).toBeInTheDocument()
+        //getbydisplayvalue
+        const byvalue=screen.getByDisplayValue('praveen')
+        expect(byvalue).toBeInTheDocument()
+        //getbytext
+        const getele=screen.getByText("this is praveen")
+        expect(getele).toBeInTheDocument()
         const comb=screen.getByRole('combobox')
         expect(comb).toBeInTheDocument()
         const check=screen.getByRole('checkbox')
         expect(check).toBeInTheDocument()
+        //getbyplaceholder
+        const placeh=screen.getByPlaceholderText('fullname')
+        expect(placeh).toBeInTheDocument()
+        //byaltvalue
+        const altvalue=screen.getByAltText("images")
+        expect(altvalue).toBeInTheDocument()
+        //bytestid
+        const testid=screen.getByTestId("test")
+        expect(testid).toBeInTheDocument()
     })
+  test("button",()=>{
+    render(<TestHello/>)
+    const btn=screen.getByRole('button',{
+        name:"Login"
+    })
+    expect(btn).toBeInTheDocument()
+
+  })
+
+  test("start learning",async ()=>{
+    render(<TestHello/>)
+    const startlearning=await screen.findByRole('button',{
+        name:'start learning'
+    },
+
+    {
+      timeout:2001,
+    }
+  )
+    expect(startlearning).toBeInTheDocument()
+
+  })
 })
